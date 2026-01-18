057a3112daa52fb1b1a7923b3d34bd8acb9ea1ea
Jaume Sanchez <the.spite@gmail.com>
2026-01-18 22:09:55 +0000

    Update main.js

--- 2/main.js
+++ 2/main.js
@@ -73,7 +73,7 @@ gui.addSlider("Metalness", params.metalness, 0, 1, 0.01, (metalness) => {
 gui.addButton("Random", randomize);
 gui.addSeparator();
 gui.addText(
-  "<p>Press R to shuffle the objects.</p><p>Press Space to toggle rotation.</p><p>Press Tab to toggle this GUI.</p>"
+  "<p>Press R to randomize the positions.</p><p>Press Space to toggle the animation.</p><p>Press Tab to toggle this GUI.</p>"
 );
 gui.show();
 
